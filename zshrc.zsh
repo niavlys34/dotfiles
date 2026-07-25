e8cb6ae93d3115a1d918dec2cef1e5af584a6563
Sylvain <sylvain@ecomail.fr>
2026-07-25 14:21:10 +0200

    zshrc et alias de base

---
+++ zshrc.zsh
@@ -0,0 +1,2 @@
+[[ -f ~/zshrc_local.zsh ]] && source ~/zshrc_local.zsh
+[[ -f ~/dotfiles/zshrc_aliases.zsh ]] && source ~/dotfiles/zshrc_aliases.zsh
---
+++ zshrc_aliases.zsh
@@ -0,0 +1,13 @@
+# Commandes générales
+alias c='clear'
+alias ls='ls -laG'
+alias ww='which'
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias h='cd ~'
+
+# Environnement virtuel
+alias ve='source ./.venv/bin/activate'
+alias dve='deactivate'
